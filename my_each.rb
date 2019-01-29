9db4816a32ee54c3e26d5f916b2864e2696afb54
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-29 18:02:55 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(array)# put argument(s) here
   # code here
+  i = 0 #creating a counter and that counter can be used for our index. 
+  while i < array.length 
+    yield (array[i]) #the goal is to yield each element
+  i = i + 1 #incrementing our counter by 1 
 end
+  array #returning the original array
+end 
+
+#def hello_t(array)
+ # i = 0
+ 
+  #while i < array.length
+   # i = i + 1
+  #end
+#end
